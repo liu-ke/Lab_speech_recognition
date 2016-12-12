9121e0bb5c0d7ba5237fc660b9bd90aed155c1cb
LiuKe <kennethliuke@gmail.com>
2016-12-12 20:01:48 +0100

    vioce detection completed

--- frameseg.m
+++ frameseg.m
@@ -1,4 +1,4 @@
-function [train_frames,test_frames]=frameseg(audio_input)
+function [frames]=frameseg(audio_input)
 %to segment the audio input into several frames, with each one 320 samples
 num=floor(length(audio_input)/160)-1;
 frames=zeros(320,num);
@@ -7,7 +7,6 @@ for i=1:num
     frames(:,i)=audio_input(j+1:j+320,1);
     j=j+160;
 end
-train_frames=frames(:,1:floor(num*0.8));
-test_frames=frames(:,floor(num*0.8)+1:end);
+
 
 
--- main.m
+++ main.m
@@ -1,8 +1,9 @@
-
+clc;
+clear all;
 speech_dirpath='F:\Courses\INFOTECH\2017ws\DPRLAB\patRecDat\forStudents\timit\test';%path of training data
 speech_dir=dir(speech_dirpath);%get information (dr1-9) under training data speech_dir
 name_res=zeros(160*1);
-num=0;
+num_person=0;
 for i=1:length(speech_dir)
     index=regexp(speech_dir(i).name,'dr+\d');                   %index=1 if dr_dirpath(i).name matched to dr0-dr9
     if(~size(index))
@@ -18,19 +19,26 @@ for i=1:length(speech_dir)
         dat_dirpath=fullfile(speech_dirpath,speech_dir(i).name,dr_dir(j).name); %get paths of ppl's name
         dat_dir=dir(dat_dirpath);                                               %get the information under ppl's name
         name=regexp(dat_dirpath,'[fm]\w\w\w+\d','match');                          %get the speaker's name
-        num=num+1;
-        content=[];
+        num_person=num_person+1;                                            %number of person
+        num_wav=0;                                                          %number of person's file
+        train_frames=[];
+        test_frames=[];
         for k=1:length(dat_dir)
             index=regexp(dat_dir(k).name,'s+\w+\S');
             if(~size(index))
                 continue;
             end
             wav_path=fullfile(speech_dirpath,speech_dir(i).name,dr_dir(j).name,dat_dir(k).name); %get path of each wav
-            temp=audioread(wav_path);                                                        %get the wav signals
-            content=[content;temp];                                                         %collect all wav files of one person
+            content=audioread(wav_path);                                                        %get the wav signals of one file
+            frames=frameseg(content);
+            voiced_frames=voicedetection(frames);
+            num_wav=num_wav+1;
+            if num_wav <= 8
+                train_frames=[train_frames,voiced_frames];
+            else
+                test_frames=[test_frames,voiced_frames];
+            end
         end
-        [train_frames,test_frames]=frameseg(content);
-        voiced_frames=voicedetection(train_frames);
         
     end
 end
